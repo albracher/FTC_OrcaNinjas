eb5ad8e5fa3242ac80d1983f1b52f2a860ee8ae7
aadhi <byeman20@gmail.com>
2017-10-29 14:06:09 -0700

    servo claw

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HolonomicTeleOP_POV.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HolonomicTeleOP_POV.java
@@ -117,7 +117,7 @@ public class HolonomicTeleOP_POV extends LinearOpMode {
 
 
             // Use gamepad left & right Bumpers to open and close the claw
-            /*if (gamepad1.right_bumper)
+            if (gamepad1.right_bumper)
                 clawOffset += CLAW_SPEED;
             else if (gamepad1.left_bumper)
                 clawOffset -= CLAW_SPEED;
@@ -126,10 +126,10 @@ public class HolonomicTeleOP_POV extends LinearOpMode {
             clawOffset = Range.clip(clawOffset, -0.5, 0.5);
             robot.leftClaw.setPosition(robot.MID_SERVO + clawOffset);
             robot.rightClaw.setPosition(robot.MID_SERVO - clawOffset);
-            */
+            
 
             // Use gamepad buttons to move arm up (Y) and down (A)
-            if (gamepad1.right_bumper)
+            /*if (gamepad1.right_bumper)
                 clawOffset  += CLAW_SPEED;
             else if (gamepad1.left_bumper)
                 clawOffset -= CLAW_SPEED;
@@ -138,6 +138,7 @@ public class HolonomicTeleOP_POV extends LinearOpMode {
             clawOffset = Range.clip(clawOffset, -0.5, 0.5);
             robot.scissorclaw1.setPosition(robot.MID_SERVO + clawOffset);
             robot.scissorclaw2.setPosition(robot.MID_SERVO - clawOffset);
+            */
 
             // Send telemetry message to signify robot running;
 
